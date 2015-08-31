39fa16b0891f93a67ccbe2d7d0843bd3aece829f
Ryan Hughes <ryan@oodle.io>
2015-08-31 14:53:56 -0400

    Added vim installer to the main script

--- vim/install.sh
+++ vim/install.sh
@@ -5,8 +5,16 @@
 # This installs some of the common dependencies needed (or at least desired)
 # using Vim.
 
+echo "Applying some Vim settings..."
+
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Install the plugins
 vim +PluginInstall +qall
+
+# Make Vim directories
+mkdir ~/.vim/backup
+mkdir ~/.vim/swap
+mkdir ~/.vim/undo
+mkdir ~/.vim/views
