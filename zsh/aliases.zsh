0944e72926395c9410ff81bab29806bd5caace80
Ryan Hughes <ryan@oodle.io>
2015-10-21 15:04:23 -0400

    Added an alias for hosts

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,4 @@ alias reload!='. ~/.zshrc'
 alias caskrepo="$(brew --repository)"/Library/Taps/caskroom/homebrew-cask
 alias dev="~/Development"
 alias weather="wego"
+alias hosts="sudo vim /etc/hosts"
