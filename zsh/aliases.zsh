2de061dd16b088f41386b9628ae27f8c135cda7d
Ryan Hughes <ryan@oodle.io>
2015-10-23 16:43:36 -0400

    Added an alias for Cliist

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,4 @@ alias caskrepo="$(brew --repository)"/Library/Taps/caskroom/homebrew-cask
 alias dev="~/Development"
 alias weather="wego"
 alias hosts="sudo vim /etc/hosts"
+alias todos="~/Development/Cliist/cliist.py"
