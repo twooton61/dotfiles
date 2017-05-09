a0db9db3eb4dbe40603bb2a9dff3e879426a31ad
Terry Wooton <terry@oodle.io>
2017-05-09 15:00:05 -0400

    Syntastic plugin, shortcuts for Projects and guetzli

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,3 +6,5 @@ alias hosts="sudo vim /etc/hosts"
 alias todos="~/Development/Cliist/cliist.py"
 alias killrails="kill -9 $(lsof -i tcp:3000 -t)"
 alias killpostgre="launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist && pg_ctl restart -D /usr/local/var/postgres && launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
+alias p="cd ~/Projects"
+alias gu='function _guetzli(){ guetzli --quality 84 $1 $1 }; _guetzli' 
