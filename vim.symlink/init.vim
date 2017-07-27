122e8ee0251140b601dc3b19b8fba0b3adb37839
Terry Wooton <terry@oodle.io>
2017-07-27 13:45:21 -0400

    update vimrc and aliases

--- vim.symlink/init.vim
+++ vim.symlink/init.vim
@@ -1,1 +1,3 @@
-/Users/terry/.vimrc
+set runtimepath+=~/.vim,~/.vim/after
+set packpath+=~/.vim
+source ~/.vimrc
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,6 +6,9 @@ alias hosts="sudo vim /etc/hosts"
 alias todos="~/Development/Cliist/cliist.py"
 alias killrails="kill -9 $(lsof -i tcp:3000 -t)"
 alias killpostgre="launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist && pg_ctl restart -D /usr/local/var/postgres && launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
-alias p="cd ~/Projects"
 alias gu='function _guetzli(){ guetzli --quality 84 $1 $1 }; _guetzli' 
 alias wt="cd wp-content/themes"
+
+alias p="cd ~/Projects"
+alias parmour="cd ~/Projects/jmfg-armour/wp-content/themes/themetrust-create"
+alias pferno="cd ~/Projects/ferno-kentico/pattern-lab"
