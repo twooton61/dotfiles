3618d4ed3298c0064809e839d1ac3bb44ef91fb8
Terry Wooton <terry@oodle.io>
2017-07-28 09:41:52 -0400

    udpate aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,5 +10,4 @@ alias gu='function _guetzli(){ guetzli --quality 84 $1 $1 }; _guetzli'
 alias wt="cd wp-content/themes"
 
 alias p="cd ~/Projects"
-alias parmour="cd ~/Projects/jmfg-armour/wp-content/themes/themetrust-create"
-alias pferno="cd ~/Projects/ferno-kentico/pattern-lab"
+alias vim="nvim"
